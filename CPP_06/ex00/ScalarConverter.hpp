a23953a378bcb0f11cac2c4e65efedffd7517ba4
ubazzane <urbanobazz@gmail.com>
2024-06-05 12:22:46 +0200

    started ex00

---
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 12:15:12 by ubazzane          #+#    #+#             */
+/*   Updated: 2024/06/05 12:22:26 by ubazzane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_H
+#define SCALAR_CONVERTER_H
+
+#include <iostream>
+#include <string>
+
+class ScalarConverter{
+
+}
+
+
+#endif
