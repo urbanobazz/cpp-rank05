c9e01b8222e5bd1221d8b5f4f1c2e69c5f5f44ea
ubazzane <urbanobazz@gmail.com>
2024-06-05 19:02:11 +0200

    cpp06 fixed ex00

--- CPP_06/ex00/ScalarConverter.cpp
+++ CPP_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:15:30 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/05 16:19:09 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/05 18:59:47 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ void ScalarConverter::convert(std::string const &input)
 	int num = std::atoi(input.c_str());
 	// Char
 	std::cout << "Char: ";
+	if (input.size() == 1 && input[0] != '0')
+		num = static_cast<int>(input[0]);
 	if (num == 0 && !(input.size() == 1 && input[0] == '0'))
 		std::cout << "impossible" << std::endl;
 	else if (std::isprint(num))
@@ -46,7 +48,12 @@ void ScalarConverter::convert(std::string const &input)
 	std::cout << "Float: ";
 	char *end;
 	float f = std::strtof(input.c_str(), &end);
-	if (end == input.c_str() || (f == 0.0f && input != "0" && input != "0.0"))
+	if (input.size() == 1 && input[0] != '0')
+	{
+		f = static_cast<float>(input[0]);
+		std::cout << std::fixed << std::setprecision(1) << f << "f" << std::endl;
+	}
+	else if (end == input.c_str() || (f == 0.0f && input != "0" && input != "0.0"))
 		std::cout << "impossible" << std::endl;
 	else
 		std::cout << std::fixed << std::setprecision(1) << f << "f" << std::endl;
@@ -54,7 +61,12 @@ void ScalarConverter::convert(std::string const &input)
 	//Double
 	std::cout << "Double: ";
 	double d = strtod(input.c_str(), &end);
-	if (end == input.c_str() || (d == 0.0 && input != "0" && input != "0.0"))
+	if (input.size() == 1 && input[0] != '0')
+	{
+		d = static_cast<double>(input[0]);
+		std::cout << std::fixed << std::setprecision(1) << d << std::endl;
+	}
+	else if (end == input.c_str() || (d == 0.0 && input != "0" && input != "0.0"))
 		std::cout << "impossible" << std::endl;
 	else
 		std::cout << std::fixed << std::setprecision(1) << d << std::endl;
