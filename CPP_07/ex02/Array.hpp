7ceb5cd54f37b291ff970c9856de22d07ab29485
Urbano Jr <urbanobazz@gmail.com>
2024-06-06 16:15:33 +0200

    cpp07-ex02 done

--- CPP_07/ex02/Array.hpp
+++ CPP_07/ex02/Array.hpp
@@ -6,7 +6,90 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:01:44 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/06 14:01:45 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:55:00 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <exception>
+
+template <typename T>
+class Array
+{
+	public:
+		Array(void);
+		Array(unsigned int n);
+		Array(Array const &other);
+		~Array(void);
+
+		Array &operator=(Array const &other);
+		T &operator[](unsigned int index);
+
+		unsigned int size(void) const;
+
+		class OutOfBoundException: public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+
+	private:
+		T *_array;
+		unsigned int _size;
+};
+
+template <typename T>
+Array<T>::Array(void): _array(NULL), _size(0) {}
+
+template <typename T>
+Array<T>::Array(unsigned int n): _array(new T[n]), _size(n) {}
+
+template <typename T>
+Array<T>::Array(Array const &other): _array(NULL), _size(0)
+{
+	*this = other;
+}
+
+template <typename T>
+Array<T>::~Array(void)
+{
+	if (this->_array)
+		delete[] this->_array;
+}
+
+template <typename T>
+Array<T> &Array<T>::operator=(Array const &other)
+{
+	if (this->_array)
+		delete[] this->_array;
+	this->_array = new T[other._size];
+	this->_size = other._size;
+	for (unsigned int i = 0; i < this->_size; i++)
+		this->_array[i] = other._array[i];
+	return (*this);
+}
+
+template <typename T>
+T &Array<T>::operator[](unsigned int index)
+{
+	if (index >= this->_size)
+		throw Array<T>::OutOfBoundException();
+	return (this->_array[index]);
+}
+
+template <typename T>
+unsigned int Array<T>::size(void) const
+{
+	return (this->_size);
+}
+
+template <typename T>
+const char *Array<T>::OutOfBoundException::what() const throw()
+{
+	return ("Array index out of bounds!");
+}
+
+#endif
--- CPP_07/ex02/main.cpp
+++ CPP_07/ex02/main.cpp
@@ -6,7 +6,66 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:01:49 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/06 14:01:49 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/06 16:14:19 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Array.hpp"
+
+int main(void)
+{
+	Array<int> intArray(5);
+	Array<std::string> stringArray(3);
+	Array<float> floatArray(8);
+
+	for (unsigned int i = 0; i < intArray.size(); i++)
+		intArray[i] = i + 1;
+	for (unsigned int i = 0; i < stringArray.size(); i++)
+		stringArray[i] = "Hello, World!";
+	for (unsigned int i = 0; i < floatArray.size(); i++)
+		floatArray[i] = i + 0.5;
+
+	std::cout << "======================== IntArray ============================ " << std::endl;
+	std::cout << "Array: ";
+	for (unsigned int i = 0; i < intArray.size(); i++)
+		std::cout << intArray[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "======================== StringArray ========================= " << std::endl;
+	std::cout << "Array: ";
+	for (unsigned int i = 0; i < stringArray.size(); i++)
+		std::cout << stringArray[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "======================== FloatArray ========================== " << std::endl;
+	std::cout << "Array: ";
+	for (unsigned int i = 0; i < floatArray.size(); i++)
+		std::cout << floatArray[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "======================== Exception =========================== " << std::endl;
+	std::cout << "Trying to access out of bound index of intArray: " << std::endl;
+	try
+	{
+		std::cout << intArray[6] << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Exception: " << e.what() << std::endl;
+	}
+	std::cout << "===================== Empty array test =======================" << std::endl;
+	Array<int> emptyArray;
+
+	std::cout << "Array size: " << emptyArray.size() << std::endl;
+	emptyArray = intArray;
+	std::cout << "Array size after assignment: " << emptyArray.size() << std::endl;
+	std::cout << "Array: ";
+	for (unsigned int i = 0; i < emptyArray.size(); i++)
+		std::cout << emptyArray[i] << " ";
+	std::cout << std::endl;
+
+	std::cout << "==============================================================" << std::endl;
+
+
+	return (0);
+}
