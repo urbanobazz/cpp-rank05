24e604aa8eead3d5fbd0d1e31518f54bb7904ae0
Urbano Jr <urbanobazz@gmail.com>
2024-06-06 15:40:59 +0200

    cpp07 ex01 done

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -6,7 +6,36 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:00:33 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/06 14:00:34 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/06 15:36:59 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "iter.hpp"
+#include "print.hpp"
+#include <string>
+
+int main(void)
+{
+	int		int_array[5] = {1, 2, 3, 4, 5};
+	float	float_array[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
+	char	char_array[5] = {'a', 'b', 'c', 'd', 'e'};
+	std::string string_array[5] = {"Hello", "World", "42", "Berlin", "C++"};
+
+	std::cout << "Int array:" << std::endl;
+	iter(int_array, 5, print);
+	std::cout << std::endl;
+
+	std::cout << "Float array:" << std::endl;
+	iter(float_array, 5, print);
+	std::cout << std::endl;
+
+	std::cout << "Char array:" << std::endl;
+	iter(char_array, 5, print);
+	std::cout << std::endl;
+
+	std::cout << "String array:" << std::endl;
+	iter(string_array, 5, print);
+	std::cout << std::endl;
+
+	return (0);
+}
--- CPP_07/ex01/print.hpp
+++ CPP_07/ex01/print.hpp
@@ -1,24 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   iter.hpp                                           :+:      :+:    :+:   */
+/*   print.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/06 14:00:45 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/06 15:27:43 by ubazzane         ###   ########.fr       */
+/*   Created: 2024/06/06 15:28:27 by ubazzane          #+#    #+#             */
+/*   Updated: 2024/06/06 15:37:05 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ITER_HPP
-# define ITER_HPP
+#ifndef PRINT_HPP
+# define PRINT_HPP
 
 # include <iostream>
 
 template <typename T>
-void	iter(T *array, size_t size, void (*func)(T const &))
+void	print(T const &elem)
 {
-	for (size_t i = 0; i < size; i++)
-		func(array[i]);
+	std::cout << elem << std::endl;
 }
+
 #endif
