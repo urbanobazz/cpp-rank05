a977eb40011c9e700f2cb88e5a6e25246a71b225
Urbano Jr <urbanobazz@gmail.com>
2024-06-07 16:27:56 +0200

    cpp08 ex00 done

--- CPP_08/ex00/easyfind.hpp
+++ CPP_08/ex00/easyfind.hpp
@@ -6,7 +6,24 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:45:19 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/07 15:45:20 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/07 16:14:42 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>
+#include <exception>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::exception();
+	return (it);
+}
+
+#endif
--- CPP_08/ex00/main.cpp
+++ CPP_08/ex00/main.cpp
@@ -6,7 +6,48 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:45:02 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/07 15:45:03 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/07 16:27:37 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <deque>
+
+int main(void)
+{
+	std::vector<int> vec;
+	std::list<int> lst;
+	std::deque<int> deq;
+
+	for (int i = 0; i < 10; i++)
+	{
+		vec.push_back(i);
+		lst.push_back(i);
+		deq.push_back(i);
+	}
+	std::cout << "=============== Value found test ==================" << std::endl;
+	try
+	{
+		std::cout << "Vector: " << *easyfind(vec, 5) << std::endl;
+		std::cout << "List: " << *easyfind(lst, 5) << std::endl;
+		std::cout << "Deque: " << *easyfind(deq, 5) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Value not found" << std::endl;
+	}
+	std::cout << "============ Index out of bounds test =============" << std::endl;
+	try
+	{
+		std::cout << "Vector: " << *easyfind(vec, 10) << std::endl;
+		std::cout << "List: " << *easyfind(lst, 10) << std::endl;
+		std::cout << "Deque: " << *easyfind(deq, 10) << std::endl;
+	}
+	catch (std::exception &e)
+	{
+		std::cerr << "Value not found" << std::endl;
+	}
+	return (0);
+}
