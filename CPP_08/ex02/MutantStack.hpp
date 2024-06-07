57afc4c8557b0c84e0e559e3177d47d5d62417bd
Urbano Jr <urbanobazz@gmail.com>
2024-06-07 20:58:16 +0200

    cpp08 ex02 done

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -6,7 +6,33 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:47:02 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/07 15:47:02 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/07 20:37:55 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <deque>
+# include <iterator>
+
+template <typename T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container> {
+	public:
+		MutantStack(void) : std::stack<T>() {}
+		MutantStack(MutantStack const &other) : std::stack<T, Container>(other) {}
+		virtual ~MutantStack(void) {}
+
+		MutantStack &operator=(MutantStack const &other) {
+			if (this != &other)
+				std::stack<T>::operator=(other);
+			return *this;
+		}
+
+		typedef typename std::stack<T, Container>::container_type::iterator iterator;
+		iterator begin(void) { return this->c.begin(); }
+		iterator end(void) { return this->c.end(); }
+};
+
+#endif
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,104 @@
 /*   By: ubazzane <ubazzane@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 15:46:34 by ubazzane          #+#    #+#             */
-/*   Updated: 2024/06/07 15:46:35 by ubazzane         ###   ########.fr       */
+/*   Updated: 2024/06/07 20:57:47 by ubazzane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <stack>
+#include <list>
+#include <vector>
+#include "MutantStack.hpp"
+
+int main()
+{
+	std::cout << "===================== MutantStack ===================" << std::endl;
+	MutantStack<int> mstack;
+
+	mstack.push(5);
+	mstack.push(17);
+
+	std::cout << "Top: " << mstack.top() << std::endl;
+
+	mstack.pop();
+
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+
+	++it;
+	--it;
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "===================== std::list ===================" << std::endl;
+	std::list<int> list;
+
+	list.push_back(5);
+	list.push_back(17);
+
+	std::cout << "Top: " << list.back() << std::endl;
+
+	list.pop_back();
+
+	std::cout << "Size: " << list.size() << std::endl;
+
+	list.push_back(3);
+	list.push_back(5);
+	list.push_back(737);
+	list.push_back(0);
+
+	std::list<int>::iterator it2 = list.begin();
+	std::list<int>::iterator ite2 = list.end();
+
+	++it2;
+	--it2;
+
+	while (it2 != ite2) {
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::list<int> l(list);
+
+	std::cout << "===================== std::vector ===================" << std::endl;
+	std::vector<int> vec;
+
+	vec.push_back(5);
+	vec.push_back(17);
+
+	std::cout << "Top: " << vec.back() << std::endl;
+
+	vec.pop_back();
+
+	std::cout << "Size: " << vec.size() << std::endl;
+
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(737);
+	vec.push_back(0);
+
+	std::vector<int>::iterator it3 = vec.begin();
+	std::vector<int>::iterator ite3 = vec.end();
+
+	++it3;
+	--it3;
+
+	while (it3 != ite3) {
+		std::cout << *it3 << std::endl;
+		++it3;
+	}
+	std::vector<int> v(vec);
+
+	std::cout << "======================== END ========================" << std::endl;
+	return 0;
+}
